adf620e5258b141a2e42af86187d432399136b64
Romaine C. Martin III <romeyro@gmail.com>
2012-01-26 21:33:16 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blknotes::Application.config.secret_token = 'b02b9414fc1d38380e48a278e1d9334c1132479905b5b3ba58174602091a4cc24e36d53d988db1065fa841a179b528a2c863e94a12edd5c9f6cf2366d721f2dc'
