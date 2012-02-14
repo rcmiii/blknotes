7d970a6efb23e3ae722af4ead32cc459c956de91
Romaine C. Martin III <romeyro@gmail.com>
2012-02-13 20:25:14 -0600

    more stuff for listings/etc

--- app/controllers/admin/listings_controller.rb
+++ app/controllers/admin/listings_controller.rb
@@ -9,13 +9,15 @@ class Admin::ListingsController < ApplicationController
   
   def create
     @listing = Listing.new(params[:listing])
-    address = @listing.address1 + ', ' + @listing.city.name + ', ' + @listing.city.state + ' ' + @listing.zip
+    this_city = City.find(@listing.city_id)
+    address = @listing.address1 + ', ' + this_city.name + ', ' + this_city.state + ' ' + @listing.zip.to_s
     address.sub(' ', '+')
     coords = RestClient.get 'http://maps.googleapis.com/maps/api/geocode/json', {:params => {'address' => address, 'sensor' => 'false'}}
     coords = ActiveSupport::JSON.decode(coords)
     @listing.lat = coords['results'][0]['geometry']['location']['lat']
     @listing.long = coords['results'][0]['geometry']['location']['lng'] 
     @listing.save
+    render :layout => "admin"
   end
   
   def edit
--- db/migrate/20120213070654_add_category_to_listing.rb
+++
@@ -1,4 +0,0 @@
-class AddCategoryToListing < ActiveRecord::Migration
-  def change
-  end
-end
