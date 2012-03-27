fdcda4c1af08f7ae46424b8680647ae73f502572
Romaine C. Martin III <romeyro@gmail.com>
2012-03-27 01:49:25 -0500

    removed assets that shouldn't be there

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
   def show
     @city     = City.where(:name => params[:city_id].capitalize).first 
-    @listings = Category.where(:name => params[:id].capitalize).first.listings.page(params[:page])
+    @listings = Category.where(:name => params[:id].capitalize).first.listings.where(:city_id => @city.id).page(params[:page])
     if request.xhr?
       render :layout => nil
     else
