913ff1035940da0ce9dc83bd207825a7938eca7d
Romaine C. Martin III <romeyro@gmail.com>
2012-03-27 01:25:46 -0500

    images to s3

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ActiveRecord::Base
   has_and_belongs_to_many :cities
-  has_attached_file :image, :styles => { :main => "500x400>"}
+  has_attached_file :image, 
+    :styles => { :main => "500x400>"},
+    :storage        => :s3,
+    :s3_credentials => "#{Rails.root}/config/aws_config.yml",
+    :bucket         => "blknotes";
 end
--- app/models/spotlight.rb
+++ app/models/spotlight.rb
@@ -1,4 +1,9 @@
 class Spotlight < ActiveRecord::Base
   has_and_belongs_to_many :cities
-  has_attached_file :image, :styles => { :main => "500x400>"}
+  has_attached_file :image,
+    :styles => { :main => "500x400>"},
+    :storage        => :s3,
+    :s3_credentials => "#{Rails.root}/config/aws_config.yml",
+    :bucket         => "blknotes";
+  
 end
